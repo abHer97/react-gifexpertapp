9eeeaddc626d55a099bb82247dfd898fd101c142
Abel Herrera <abelherrera9720@gmail.com>
2020-08-24 20:29:02 -0500

    Docs added

---
+++ docs/precache-manifest.e38bb3ac07aea7c23318cd614a011efb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2ccc13da3db3f13a43c6df6e63b13a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af01e83d02e2308f08fb",
+    "url": "/static/css/main.5d8f8407.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af01e83d02e2308f08fb",
+    "url": "/static/js/main.787ce48f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
